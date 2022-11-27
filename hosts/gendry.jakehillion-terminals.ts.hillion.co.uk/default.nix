0921c9f16aaa7ed6e6f6c8b51f1fdf4fd74ce1f7
Jake Hillion <jake@hillion.co.uk>
2022-11-27 18:02:53 +0000

    gendry: cleanup: move persisting data to module

--- hosts/gendry.jakehillion-terminals.ts.hillion.co.uk/default.nix
+++ hosts/gendry.jakehillion-terminals.ts.hillion.co.uk/default.nix
@@ -9,6 +9,7 @@
   imports = [
     ../../modules/common/default.nix
     ./hardware-configuration.nix
+    ./persist.nix
   ];
 
   config.boot.loader.systemd-boot.enable = true;
@@ -27,43 +28,5 @@
   ## Enable btrfs compression
   config.fileSystems."/data".options = [ "compress=zstd" ];
   config.fileSystems."/nix".options = [ "compress=zstd" ];
-
-  ## Persist files (due to tmpfs root)
-  ### Set root tmpfs to 0755
-  config.fileSystems."/".options = [ "mode=0755" ];
-
-  ### Require data at boot (to have access to host keys for agenix)
-  config.fileSystems."/data".neededForBoot = true;
-
-  ### OpenSSH Host Keys (SSH + agenix secrets)
-  config.services.openssh = {
-    hostKeys = [
-      {
-        path = "/data/system/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/data/system/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
-    ];
-  };
-
-  ### Persistent directory symlinks
-  config.systemd.tmpfiles.rules = [
-    #### Persistent home subdirectories
-    "L /root/local - - - - /data/users/root"
-    "L /home/jake/local - - - - /data/users/jake"
-  ];
-
-  ### Persistent /etc/nixos
-  config.fileSystems."/etc/nixos" = {
-    device = "/data/users/root/repos/nixos";
-    options = [ "bind" ];
-  };
-
-  ### Persistent zsh history
-  config.programs.zsh.histFile = lib.mkForce "$HOME/local/.zsh_history";
 }
 
