6bdfc37391034852ca04403da5989f3e082e62c3
Jake Hillion <jake@hillion.co.uk>
2023-01-28 21:06:42 +0000

    awesome: add default rc.lua

--- modules/desktop/awesome/default.nix
+++ modules/desktop/awesome/default.nix
@@ -5,4 +5,9 @@
     enable = true;
     windowManager.awesome.enable = true;
   };
+
+  home-manager.users.jake.xdg.configFile."awesome/rc.lua" = {
+    source = ./rc.lua;
+    onChange = with pkgs; "echo 'awesome.restart()' | ${awesome}/bin/awesome-client";
+  };
 }
