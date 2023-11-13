cd79b1e60a6dab2cf98dff4412881867f5553205
Jake Hillion <jake@hillion.co.uk>
2023-11-13 10:40:46 +0000

    tywin: increase storj allocation to 1250GB

--- hosts/tywin.storage.ts.hillion.co.uk/default.nix
+++ hosts/tywin.storage.ts.hillion.co.uk/default.nix
@@ -221,7 +221,7 @@
             externalAddress = "d${toString index}.tywin.storj.hillion.co.uk:${toString (28967 + index)}";
             consoleAddress = "100.115.31.91:${toString (14002 + index)}";
 
-            storage = "1000GB";
+            storage = "1250GB";
           };
         };
         instances = builtins.genList (x: x) 4;
