cfec181d0dfe7204848408ce30c80271e8f7c802
Jake Hillion <jake@hillion.co.uk>
2024-04-20 23:54:58 +0100

    deploy-rs: initial setup

--- flake.nix
+++ flake.nix
@@ -13,11 +13,15 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     impermanence.url = "github:nix-community/impermanence/master";
+
+    deploy-rs.url = "github:serokell/deploy-rs/master";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
+    deploy-rs.inputs.utils.follows = "flake-utils";
   };
 
   description = "Hillion Nix flake";
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, agenix, home-manager, impermanence, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, agenix, home-manager, impermanence, deploy-rs, ... }@inputs: {
     nixosConfigurations =
       let
         fqdns = builtins.attrNames (builtins.readDir ./hosts);
@@ -55,6 +59,21 @@
           };
       in
       nixpkgs.lib.genAttrs fqdns mkHost;
+      
+    deploy = {
+      nodes = builtins.mapAttrs
+        (name: value: {
+          hostname = name;
+          profiles.system = {
+            user = "root";
+            remoteBuild = true;
+            path = deploy-rs.lib.aarch64-darwin.activate.nixos self.nixosConfigurations.${name};
+          };
+        })
+        self.nixosConfigurations;
+    };
+
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   } // flake-utils.lib.eachDefaultSystem (system: {
     formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
   });
