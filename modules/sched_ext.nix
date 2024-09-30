3d8a60da5bc61935f51c658c36ac457bf426950a
Jake Hillion <jake@hillion.co.uk>
2024-09-30 13:02:52 +0100

    sched_ext: bump kernel to 6.12-rc1
    Removes the custom kernel features and requires any host running
    sched_ext to pull a kernel at least 6.12. Looks at
    pkgs.unstable.linuxPackages first, if that's too old it falls back to
    pkgs.linuxPackages_latest, and if that's too old it goes for
    pkgs.unstable.linuxPackages_testing.
    
    The plan is to leave `boot.kernelPackages` alone if new enough, but
    we'll keep the assertion. Some schedulers might require more specific
    kernel constraints in the future.

--- modules/sched_ext.nix
+++ modules/sched_ext.nix
@@ -9,35 +9,12 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    boot.kernelPackages =
-      let
-        linux_6_12_pkg = { fetchFromGitHub, buildLinux, ... } @ args:
-          buildLinux (args // rec {
-            version = "6.11.0"; # trying to fix "Error: modDirVersion 6.12 specified in the Nix expression is wrong, it should be: 6.11.0", this is really a 6.12 pre-release
-            modDirVersion = version;
+    assertions = [{
+      assertion = config.boot.kernelPackages.kernelAtLeast "6.12";
+      message = "sched_ext requires a kernel >=6.12";
+    }];
 
-            src = fetchFromGitHub {
-              owner = "torvalds";
-              repo = "linux";
-              rev = "88264981f2082248e892a706b2c5004650faac54"; # sched_ext merge in 6.12 merge window
-              hash = "sha256-k+Mnzb8QFCOaoavBa16XHPVPz7vX2YcuTQpvec8sR6k=";
-            };
-
-            extraConfig = ''
-              BPF y
-              BPF_EVENTS y
-              BPF_JIT y
-              BPF_SYSCALL y
-              DEBUG_INFO_BTF y
-              FTRACE y
-              SCHED_CLASS_EXT y
-            '';
-
-            ignoreConfigErrors = true;
-          } // (args.argsOverride or { }));
-        linux_6_12 = pkgs.callPackage linux_6_12_pkg { };
-      in
-      pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_6_12);
+    boot.kernelPackages = if pkgs.linuxPackages.kernelAtLeast "6.12" then pkgs.linuxPackages else (if pkgs.linuxPackages_latest.kernelAtLeast "6.12" then pkgs.linuxPackages_latest else pkgs.unstable.linuxPackages_testing);
   };
 }
 
