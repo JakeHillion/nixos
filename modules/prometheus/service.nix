6f5b9430c963177c8de39f1ca2fb53be608b4f33
Jake Hillion <jake@hillion.co.uk>
2024-10-12 20:16:23 +0100

    prometheus: add alert for resilio sync going down

--- modules/prometheus/service.nix
+++ modules/prometheus/service.nix
@@ -23,6 +23,20 @@ in
           targets = builtins.map (x: "${x}:9000") (builtins.attrNames (builtins.readDir ../../hosts));
         }];
       }];
+
+      rules = [
+        ''
+          groups:
+          - name: service alerting
+            rules:
+            - alert: ResilioSyncDown
+              expr: node_systemd_unit_state{ name = 'resilio.service', state != 'active' } > 0
+              for: 10m
+              annotations:
+                summary: "Resilio Sync systemd service is down"
+                description: "The Resilio Sync systemd service is not active on instance {{ $labels.instance }}."
+        ''
+      ];
     };
 
     services.caddy = {
