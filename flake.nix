3a0c8effbbf2971bf9beb40dd7183ef168726e6f
Jake Hillion <jake@hillion.co.uk>
2024-10-23 21:38:11 +0100

    flake: use generic branch from forked nixpkgs
    Rather than pulling a specific feature branch into `nixpkgs-unstable`, pull a branch for `nixos-unstable`. Then we can cherry pick and rebase multiple features in the `nixpkgs` fork and keep up more easily with upstream.
    
    The preferred method here would be a list of PRs or URLs to patch into the flake input, but that doesn't seem to be an option.
    
    This change adds in the resilio gid PR, but is otherwise unchanged.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:JakeHillion/nixpkgs/scx_layered_init";
+    nixpkgs-unstable.url = "github:JakeHillion/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
