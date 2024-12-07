2210a4ea60b77cca20c4b15514a5cb4c1a0a33bd
Jake Hillion <jake@hillion.co.uk>
2024-12-07 22:09:48 +0000

    merlin: enable kdeconnect

--- hosts/merlin.rig.ts.hillion.co.uk/default.nix
+++ hosts/merlin.rig.ts.hillion.co.uk/default.nix
@@ -48,6 +48,12 @@
     boot.initrd.kernelModules = [ "amdgpu" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
 
+    # TODO: move and impermanence me
+    programs.kdeconnect.enable = true;
+    home-manager.users."jake".services.kdeconnect.enable = true;
+    hardware.uinput.enable = true;
+    users.users."jake".extraGroups = [ "uinput" ];
+
     # Networking
     networking = {
       interfaces.enp171s0.name = "eth0";
@@ -60,11 +66,20 @@
       allowedTCPPorts = lib.mkForce [
         22 # SSH
       ];
+      allowedTCPPortRanges = lib.mkForce [ ];
       allowedUDPPorts = lib.mkForce [ ];
+      allowedUDPPortRanges = lib.mkForce [ ];
+
       interfaces = {
         eth0 = {
           allowedTCPPorts = lib.mkForce [ ];
+          allowedTCPPortRanges = lib.mkForce [
+            { from = 1714; to = 1764; } # KDE Connect
+          ];
           allowedUDPPorts = lib.mkForce [ ];
+          allowedUDPPortRanges = lib.mkForce [
+            { from = 1714; to = 1764; } # KDE Connect
+          ];
         };
       };
     };
