1215cb1717ba0e1b61a0bb38880399c7bbc28978
Jake Hillion <jake@hillion.co.uk>
2025-01-02 01:03:33 +0000

    iceman.tick: init host

---
+++ hosts/iceman.tick.neb.jakehillion.me/default.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, nixos-hardware, ... }:
+
+{
+  imports = [
+    "${nixos-hardware}/raspberry-pi/4/default.nix"
+    ./hardware-configuration.nix
+  ];
+
+  config = {
+    system.stateVersion = "24.11";
+
+    networking.hostName = "iceman";
+    networking.domain = "tick.neb.jakehillion.me";
+
+    custom.defaults = true;
+
+    ## Custom Services
+    custom.locations.autoServe = true;
+
+    ## Enable zram for 2GB Pi
+    zramSwap = {
+      enable = true;
+      memoryPercent = 200;
+      algorithm = "zstd";
+    };
+
+    # Networking
+    networking.interfaces.end0.name = "eth0";
+    networking.firewall = {
+      trustedInterfaces = [ "neb.jh" ];
+      allowedTCPPorts = lib.mkForce [
+        22 # SSH
+      ];
+      allowedUDPPorts = lib.mkForce [ ];
+      interfaces = {
+        eth0 = {
+          allowedTCPPorts = lib.mkForce [ ];
+          allowedUDPPorts = lib.mkForce [ ];
+        };
+      };
+    };
+  };
+}
+
