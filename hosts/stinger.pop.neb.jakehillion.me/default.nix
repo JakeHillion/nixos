58722b1937ff6696d486d4c00055251256280b87
Jake Hillion <jake@hillion.co.uk>
2025-02-07 18:59:29 +0000

    tang: move full disk encryption configuration into module

--- hosts/stinger.pop.neb.jakehillion.me/default.nix
+++ hosts/stinger.pop.neb.jakehillion.me/default.nix
@@ -13,29 +13,23 @@
     boot.loader.efi.canTouchEfiVariables = true;
 
     boot.kernelParams = [
-      "ip=dhcp"
-
       # zswap
       "zswap.enabled=1"
       "zswap.compressor=zstd"
       "zswap.max_pool_percent=20"
     ];
-    boot.initrd = {
-      availableKernelModules = [ "r8169" ];
-      network.enable = true;
-      clevis = {
-        enable = true;
-        useTang = true;
-        devices = {
-          "disk0-crypt".secretFile = "/data/disk_encryption.jwe";
-        };
-      };
-    };
 
     custom.defaults = true;
     custom.locations.autoServe = true;
     custom.impermanence.enable = true;
     custom.services.isponsorblocktv.enable = true;
+
+    custom.tang = {
+      enable = true;
+      networkingModule = "r8169";
+      secretFile = "/data/disk_encryption.jwe";
+      devices = [ "disk0-crypt" ];
+    };
 
     hardware = {
       bluetooth.enable = true;
