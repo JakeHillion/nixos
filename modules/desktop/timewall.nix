c2b9141283647805723b414a34ef40ee1df16c5b
Jake Hillion <jake@hillion.co.uk>
2025-02-22 09:55:23 +0000

    timewall: drop back to unstable plus nixpkgs#382919

--- modules/desktop/timewall.nix
+++ modules/desktop/timewall.nix
@@ -20,31 +20,13 @@ let
       ln -fs ${configFormat.generate "timewall-config.toml" (baseConfig // config)} $out/config.toml
     '';
 
-  timewallPr141 = pkgs.unstable.timewall.overrideAttrs (oldAttrs: rec{
-    pname = "timewall";
-    version = "pr141";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "bcyran";
-      repo = "timewall";
-      rev = "0b1901a19cd2a1d59854be822b79332187e76f0d";
-      hash = "sha256-Nc9CRrr4ZTV6VKjftCPElYqWZDR1zrKRwB8JbygBClg=";
-    };
-
-    cargoDeps = oldAttrs.cargoDeps.overrideAttrs ({
-      name = "${pname}-vendor.tar.gz";
-      inherit src;
-      outputHash = "sha256-2Jy2S82UalpvsPyfk+KfW1N/VQHs89uix1mWiKB3vHU=";
-    });
-  });
-
   cache = pkgs.runCommand "timewall-cache" { } ''
     mkdir $out
 
     export TIMEWALL_CONFIG_DIR=${mkConfigDir { setter.command = ["${pkgs.coreutils}/bin/true"]; }}
     export TIMEWALL_CACHE_DIR=$out
 
-    ${timewallPr141}/bin/timewall set -v ${cfg.wallpaper}
+    ${pkgs.unstable.timewall}/bin/timewall set -v ${cfg.wallpaper}
   '';
 in
 {
@@ -92,7 +74,7 @@ in
         TIMEWALL_CONFIG_DIR = "${mkConfigDir { setter.command = [ "ln" "-fs" "%f" "current_wall" ]; }}";
       };
       script = ''
-        ${timewallPr141}/bin/timewall set -v -d
+        ${pkgs.unstable.timewall}/bin/timewall set -v -d
       '';
     };
 
@@ -105,7 +87,7 @@ in
             "TIMEWALL_CACHE_DIR=${cache}"
             "TIMEWALL_CONFIG_DIR=${mkConfigDir { setter.command = [ "${pkgs.sway}/bin/swaymsg" "output * bg %f fill" ]; }}"
           ];
-          ExecStart = "${timewallPr141}/bin/timewall set -v -d";
+          ExecStart = "${pkgs.unstable.timewall}/bin/timewall set -v -d";
         };
 
         Install.WantedBy = [ "default.target" ];
