288ee9aa002b581542fb283a2b1c80a8abbaf5b7
Jake Hillion <jake@hillion.co.uk>
2025-04-05 18:33:57 +0100

    topton: only set ip=dhcp if tang is enabled

--- models/t0-topton-1u-10g/default.nix
+++ models/t0-topton-1u-10g/default.nix
@@ -5,20 +5,25 @@
     ./disko.nix
   ];
 
-  config = {
-    ## Systemd-boot
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  config = lib.mkMerge [
+    {
+      ## Systemd-boot
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
 
-    ## Setup but don't enable tang, that depends on the device's location
-    custom.tang = {
-      networkingModule = "igc";
-      secretFile = "/data/disk_encryption.jwe";
-      devices = [ "disk0-crypt" ];
-    };
-    boot.kernelParams = [ "ip=dhcp" ];
+      ## Setup but don't enable tang, that depends on the device's location
+      custom.tang = {
+        networkingModule = "igc";
+        secretFile = "/data/disk_encryption.jwe";
+        devices = [ "disk0-crypt" ];
+      };
 
-    ## Delegation
-    custom.impermanence.enable = true;
-  };
+      ## Delegation
+      custom.impermanence.enable = true;
+    }
+
+    (lib.mkIf config.custom.tang.enable {
+      boot.kernelParams = [ "ip=dhcp" ];
+    })
+  ];
 }
