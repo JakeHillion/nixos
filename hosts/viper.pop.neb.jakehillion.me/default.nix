cf1a9bc906ebbadafbdb5397e6ebed7c623f61f3
Jake Hillion <jake@hillion.co.uk>
2025-08-23 20:21:40 +0100

    viper.pop: init host

---
+++ hosts/viper.pop.neb.jakehillion.me/default.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./disko.nix
+  ];
+
+  config = {
+    system.stateVersion = "25.05";
+
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub = {
+      enable = true;
+
+      efiSupport = true;
+      device = "nodev";
+
+      mirroredBoots = lib.mkOverride 51 [
+        {
+          devices = [ "/dev/disk/by-path/pci-0000:01:00.0-nvme-1" ];
+          path = "/boot0";
+        }
+        {
+          devices = [ "/dev/disk/by-path/pci-0000:05:00.0-nvme-1" ];
+          path = "/boot1";
+        }
+      ];
+    };
+
+    custom.defaults = true;
+
+    custom.tang = {
+      enable = true;
+      networkingModule = "r8169";
+      secretFile = "/data/disk_encryption.jwe";
+      devices = [ "disk0-crypt" "disk1-crypt" ];
+    };
+
+    custom.auto_updater.allowReboot = true;
+
+    custom.impermanence.enable = true;
+  };
+}
