eff0d57609884b0ffd4314f90fc8940f2386728c
Jake Hillion <jake@hillion.co.uk>
2025-09-09 19:29:39 +0100

    neomutt: add w3c for html rendering

--- modules/home/neomutt.nix
+++ modules/home/neomutt.nix
@@ -155,6 +155,17 @@ in
           # Sorting - newest first
           sort = "reverse-date-received";
         };
+
+        extraConfig = ''
+          # HTML email viewing with w3m
+          alternative_order text/plain text/enriched text/html
+          auto_view text/html
+
+          # Define mailcap for HTML viewing
+          set mailcap_path = ${pkgs.writeText "mailcap" ''
+            text/html; ${lib.getExe pkgs.w3m} -I %{charset} -T text/html -dump; copiousoutput;
+          ''}
+        '';
       };
 
     };
