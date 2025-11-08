79436b774de46cb1840a2caf6b25d3456314ec16
Jake Hillion <jake@hillion.co.uk>
2025-11-08 07:12:41 +0000

    macvm: pin nebula lighthouse DNS

--- hosts/macvm.lt.neb.jakehillion.me/default.nix
+++ hosts/macvm.lt.neb.jakehillion.me/default.nix
@@ -19,5 +19,12 @@
 
     # TODO: Fix btrfs subvolume rollback on boot. The boot.initrd.systemd.services.rollback
     # service does not work correctly on this VM.
+
+    # Override Nebula lighthouse addresses due to DNS resolution issues
+    networking.hosts = {
+      "138.201.252.214" = [ "boron.cx.jakehillion.me" ];
+      "185.240.111.53" = [ "home.jakehillion.me" ];
+      "80.229.251.26" = [ "home.scott.hillion.co.uk" ];
+    };
   };
 }
