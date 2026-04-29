86e40e442db1172f1d59b39fe928149697b16dd8
Jake Hillion <jake@hillion.co.uk>
2026-04-29 15:48:02 +0100

    devboxes: rotate nixcache.testquorum.dev public key

--- modules/profiles/devbox.nix
+++ modules/profiles/devbox.nix
@@ -48,7 +48,7 @@ in
         "nixcache.jakehillion.me-1:HQsjYdrcs3ilS/ngtlbTQXU4Xfsm+va5NN7yoK0wKMg="
         "ogygia.cachix.org-1:xb4bnMPeWgSP81Xs0Vl7ZU4Ez7Ul65qp/EoZ40pDaWo="
         "sched-ext.cachix.org-1:dtoM9QOUUqJs3JkmSgVoKYp9cLY0BrupOqp4DVz35/g="
-        "nixcache.testquorum.dev-1:w8eYAwlsCrkOoPWvRFZa/haM19qkHu0kAHD0zkGN+0g="
+        "nixcache.testquorum.dev-1:aS+CJF8O8Ebirc6hypMfq/061h5TJlbsej1+zUJHPec="
       ];
     };
 
