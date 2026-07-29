08a2a46d55d7d11118f53e823ef798e75fa3e879
Jake Hillion <jake@hillion.co.uk>
2026-07-29 14:25:07 +0100

    iceman: add Hetzner AX162-R host and disk model
    Adds iceman.cx.neb.jakehillion.me, a Hetzner AX162-R server (EPYC 9454P, 256 GB RAM, dual NVMe), and a new t0-hetzner-ax162-r hardware model.
    
    The model partitions both NVMe disks with an ESP each and mirrored GRUB boot, a random-encrypted swap partition per disk totalling 256 GB, and a btrfs raid1 across the two LUKS crypts holding /data and /nix. The host imports the model and configures its static Hetzner networking; modules/dns.nix reserves nebula IP 172.20.0.27.
    
    Test plan:
    - CI

---
+++ hosts/iceman.cx.neb.jakehillion.me/default.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ../../models/t0-hetzner-ax162-r
+  ];
+
+  config = {
+    system.stateVersion = "26.05";
+
+    custom.defaults = true;
+
+    ogygia.nebula = {
+      groups = [ "legacy-full-access" ];
+      pubKey = ''
+        -----BEGIN NEBULA X25519 PUBLIC KEY-----
+        UqRL8CsYJytwXQf01N/UPGUXqgCkxdrxrjDedPmEhWQ=
+        -----END NEBULA X25519 PUBLIC KEY-----
+      '';
+    };
+
+    custom.tang.enable = true;
+
+    ## Networking (Hetzner dedicated server: static addressing, no DHCP)
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_forward" = true;
+      "net.ipv6.conf.all.forwarding" = true;
+    };
+
+    networking = {
+      useDHCP = false;
+      interfaces.eth0 = {
+        ipv4.addresses = [{
+          address = "37.27.136.99";
+          prefixLength = 26;
+        }];
+        ipv6.addresses = [{
+          address = "2a01:4f9:3100:1048::2";
+          prefixLength = 64;
+        }];
+      };
+      defaultGateway = {
+        address = "37.27.136.65";
+        interface = "eth0";
+      };
+      defaultGateway6 = {
+        address = "fe80::1";
+        interface = "eth0";
+      };
+    };
+  };
+}
